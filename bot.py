c0198249f5976b6b6fedf14c7e0fbc7cb2eab6f2
thatnativekid-max <thatnativekid@gmail.com>
2026-05-17 06:01:02 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -689,13 +689,13 @@ async def operation_report(
 # STRATAGEM REPORT
 # ==================================================
 
-@bot.tree.command(name="stratagem_report")
+@bot.tree.command(name="operation_report")
 @app_commands.choices(
     mission=MISSION_CHOICES,
-    difficulty=STRATAGEM_DIFFICULTY_CHOICES,
+    difficulty=OPERATION_DIFFICULTY_CHOICES,
     gene_seed=GENE_CHOICES
 )
-async def stratagem_report(
+async def operation_report(
     interaction: discord.Interaction,
     mission: app_commands.Choice[str],
     difficulty: app_commands.Choice[str],
